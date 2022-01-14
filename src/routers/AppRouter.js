65d1ae3ee5e0e672dddcd8566f4d4f99b45d28eb
augusticor <rojascruzoscar@gmail.com>
2022-01-14 18:38:19 -0500

    Keep the state of the auth user on every screen
    - Keeps the state by using useState and useEffect hooks
    - Firebase method onAuthStateChanged
    - Loading screen while checks if user is logged in

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { login } from '../actions/auth';
 
 import LoginScreen from '../components/auth/LoginScreen';
 import JournalScreen from '../components/journal/JournalScreen';
 import AuthRouter from './AuthRouter';
+import LoadingScreen from '../components/journal/LoadingScreen';
 
 const AppRouter = () => {
+	const dispatch = useDispatch();
+
+	const [checkingLogin, setCheckingLogin] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		onAuthStateChanged(getAuth(), (user) => {
+			if (user) {
+				dispatch(login(user.uid, user.displayName));
+				setIsLoggedIn(true);
+			}
+
+			setCheckingLogin(false);
+		});
+	}, [dispatch]);
+
+	if (checkingLogin) {
+		return <LoadingScreen />;
+	}
+
 	return (
 		<BrowserRouter>
 			<Routes>
